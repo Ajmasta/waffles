ee3685715291e10e624c0ed28fd41c862bc6cb0f
ajmastaflex <adrien.moevus@mail.mcgill.ca>
2022-08-29 01:56:25 -0400

    changed contract address

--- utils/contracts/contractAddress.js
+++ utils/contracts/contractAddress.js
@@ -1,4 +1,4 @@
-export const contractAddress = "0xe0314e279FF29d6c0F7F8fEf4D38F7172159c951";
+export const contractAddress = "0x27af442324f34174713Ea0F0416E33043bE59E7e";
 
 export const openseaURL = "https://testnets-api.opensea.io";
 
